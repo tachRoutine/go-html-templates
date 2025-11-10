0d14466736a42f53864067287c314bc936500c3d
Tachera Sasi <tacherasasi@gmail.com>
2025-11-10 13:28:32 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,40 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 const port = ":8080"
 
+type Todo struct {
+    Title string
+    Done  bool
+}
+
+
+
+type TodoPageData struct {
+    PageTitle string
+    Todos     []Todo
+}
+
+
 func main(){
+	tmpl := template.Must(template.ParseFiles("templates/todo.html"))
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+		data := TodoPageData{
+            PageTitle: "My TODO list",
+            Todos: []Todo{
+                {Title: "Task 1", Done: false},
+                {Title: "Task 2", Done: true},
+                {Title: "Task 3", Done: true},
+            },
+        }
+        
+        tmpl.Execute(w,data)
 	})
 	fullAddr := fmt.Sprintf("http://localhost%s", port)
 	fmt.Println("server in running http", fullAddr)
