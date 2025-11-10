716362f8330ddb70b691324e0a3d692dc568db11
Tachera Sasi <tacherasasi@gmail.com>
2025-11-10 13:26:30 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+const port = ":8080"
+
 func main(){
-	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+	fullAddr := fmt.Sprintf("http://localhost%s", port)
+	fmt.Println("server in running http", fullAddr)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		panic(err)
+	}
 }
